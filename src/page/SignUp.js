49fe0390ed13769b6f47c024becb5132888bbe97
gloriafatia <126062226+gloriafatia@users.noreply.github.com>
2025-04-12 22:30:47 +0200

    huuu

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -65,7 +65,7 @@ const SignUp = () => {
   return (
     <div className="p-3 md:p-4">
       <div className="w-full max-w-sm bg-white m-auto flex items-center flex-col p-4">
-        {/* <h1 className='text-center text-2xl font-bold'>Sign Up</h1> */}
+        {/* <h1 className='text-center text-2xl font-bold'>Sign U6p</h1> */}
         <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md">
           <img src={signUpGif} className="w-full" />
         </div>
